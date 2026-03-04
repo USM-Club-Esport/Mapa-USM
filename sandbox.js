7ae27a8264bc5b90d44ee0cece342071de453e06
Tomas Reveron <tomyreveroncito@gmail.com>
2026-03-03 23:14:04 -0400

    feat: Implement a responsive map interface with an animated side menu and add gesture handling dependencies.

---
+++ sandbox.js
@@ -0,0 +1,9 @@
+const test = {
+  facultades: "school",
+  admin: "account-balance",
+  cafeteria: "storefront",
+  entretenimiento: "casino",
+  medico: "local-hospital",
+  biblioteca: "local-library",
+  eventos: "theater-comedy"
+};
